a73a44dc09ae77fb427c536a8b24ad12d20fb8e1
Sam Boles <samiboles@gmail.com>
2013-12-29 17:24:19 +0000

    First Draft

---
+++ Home.php
@@ -0,0 +1,13 @@
+<div class="home">
+    <div class="left">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies, ipsum sed semper iaculis, neque leo mattis neque, et porta felis risus in mi. Etiam eu mauris eget lacus ullamcorper tincidunt nec vulputate risus. Proin placerat quam rhoncus urna dapibus, in tristique eros tincidunt. Curabitur porta risus pretium adipiscing imperdiet. Praesent tempor vitae justo vitae faucibus. Aliquam luctus velit libero, sed convallis urna consectetur a. Proin quis euismod est. Vestibulum vehicula lectus eget pellentesque fermentum. Aenean porttitor dolor quis nulla pulvinar pretium. In vulputate dolor id bibendum vestibulum. Fusce malesuada est et risus imperdiet porttitor. Quisque congue luctus massa a consequat. Nulla condimentum diam sed augue venenatis tempus. Sed non vulputate orci. Aliquam erat volutpat.
+
+Curabitur in venenatis diam, in accumsan quam. Donec vestibulum nec arcu fermentum commodo. Proin vehicula iaculis eros pretium consectetur. Vivamus consequat, diam a blandit commodo, nisi massa iaculis lectus, ac aliquam ligula neque ac enim. Duis pretium elit eget elit mollis, eu fringilla justo viverra. In at augue neque. Suspendisse convallis posuere ligula, et vestibulum urna iaculis a. Phasellus porta elit a dui venenatis dignissim id quis massa.
+
+Donec non feugiat lectus. Nullam eleifend quam quis eleifend tristique. Duis pretium odio sed odio feugiat viverra. Etiam non nulla congue, rhoncus felis vel, aliquam metus. Integer non nibh porta orci dictum iaculis. Aenean metus risus, fermentum eu diam ut, feugiat suscipit odio. Donec semper ullamcorper diam, in viverra ligula vehicula vel. In et posuere urna. Morbi auctor mauris augue, vitae consectetur nibh rutrum eu. Cras convallis placerat ipsum, a mollis elit volutpat posuere.
+    </div>
+    <div class="right">
+        dfnfandsn   
+    </div>
+  <br/><br/><br/>
+</div><br>
---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+    
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/Structure.css"   />
+        
+        <script src="jquery.js"></script>
+        <title>
+            Boghall Surgery
+        </title>
+        
+    </head>
+    <body>
+        <?php
+            //require_once "dbcon.php";
+            //ob_start();
+            $register = "<li onclick=\"loadReg()\">Register</li>";
+            $login = "<li onclick=\"loadLogin()\">Login</li>";
+            $reserve = "<li onclick=\"loadRes()\">Reserved</li>";
+            $logout = "<li onclick=\"logout()\">Logout</li>";
+        ?>
+        <script>
+            function loadAbout()
+            {
+                $(".window").load("Register.php");
+            }
+            function loadHome()
+            {
+                $(".window").load("Home.php");
+            }
+            function loadContact()
+            {
+                $(".window").load("Contact.php");
+            }
+        </script>
+        <div class="head">
+            Boghall Surgery
+        </div>
+        
+        <div class="menu">
+            <ul class="men">
+                <li onclick="loadHome()">Home</li>
+                <li onclick="loadContact()">Contact</li>
+                <li onclick="loadAbout()">About</li>
+            </ul>
+        </div>
+        
+        <div class="window">
+            <?php include('Home.php');?>
+        </div>
+        
+        <img class="logo" src="img/icon.jpg"/>
+        
+    </body>
+</html>
